e403c770d3a46089df31705dec18231604fde2c6
Maciej Gierwatowski <maciej.gierwatowski@persgroep.net>
2022-07-29 15:58:20 +0200

    Drone tagger version 2

--- drone-tag/README.md
+++ drone-tag/README.md
@@ -4,25 +4,41 @@ This image runs Drone.io plugin designed to properly tag images. It will
 
 Parameters:
 
-| Parameter name              | Required | Default value | Possible values | Description                                                                                                                                      |
-|:----------------------------|:---------|:--------------|:----------------|:-------------------------------------------------------------------------------------------------------------------------------------------------|
-| `access_key`                | **yes**  | _none_        | _String_        | IAM Access key giving permissions to operate on S3 bucket                                                                                        |
-| `secret_key`                | **yes**  | _none_        | _String_        | IAM Access secret key giving permissions to operate on S3 bucket                                                                                 |
-| `fetch_version`             | **no**   | `false`       | `true`, `false` | If set on _true_ will only fetch stored version on given `s3_bucket` under given `s3_path` and save into given `local_path` file                 |
-| `local_path`                | **no**   | `.tag`        | _String_        | Path to file where will be localy stored latest version                                                                                          |
-| `region`                    | **yes**  | _none_        | _String_        | AWS region to set                                                                                                                                |
-| `s3_bucket`                 | **yes**  | _none_        | _String_        | S3 bucket which stores version numbers                                                                                                           |
-| `s3_path`                   | **yes**  | _none_        | _String_        | Path to S3 object on given bucket, which stores last version number                                                                              |
-| `s3_use_prefix_as_filename` | **no**   | `false`       | _string_        | If true, will use evaluated prefix as filename, this modifies `s3_path` to act as path (s3 object's prefix). Prefix should be set and evaluable! |
-| `tag_increment_level`       | **no**   | `0`           | `0`, `1`, `2`   | Index of number to increment: `[0]`.`[1]`.`[2]`                                                                                                  |
-| `tag_prefix`                | **no**   | _none_        | _String_        | Fixed tag prefix. If not provided, will be empty. Otherwise will create tag: `[prefix]-[version]`                                                |
-| `tag_prefix_regex`          | **no**   | _none_        | _String_        | If set, will use matching pattern from branch name to generate prefix. Overwrites `tag_prefix` if matched.                                       |
-| `log_mode`                  | **no**   | `1`           | `0`, `1`, `2`   | Log mode: 0 - warnings only, 1 - info; 2 - debug                                                                                                 |
+| Parameter name              | Required | Default value | Possible values   | Description                                                                                                                                                 |
+|:----------------------------|:---------|:--------------|:------------------|:------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| `access_key`                | **no**   | _none_        | _String_          | IAM Access key giving permissions to operate on S3 bucket                                                                                                   |
+| `secret_key`                | **no**   | _none_        | _String_          | IAM Access secret key giving permissions to operate on S3 bucket                                                                                            |
+| `fetch_only`                | **no**   | `false`       | `true`, `false`   | If set on _true_ not increment version                                                                                                                      |
+| `fetch_from`                | **no**   | `s3`          | `s3`,`local`      | By default fetches from `s3` otherwise will fetch from given local path                                                                                     |
+| `fetch_path`                | **yes**  | _none_        | _string_          | Path from which fetch last version (local path, or in case of S3 bucket `[s3-bucket]:[object_path]`)                                                        |
+| `s3_use_prefix_as_filename` | **no**   | `false`       | `true`, `false`   | If true, will use evaluated prefix as filename, this modifies `save_paths` value for `s3` key to act as directory path. Prefix should be set and evaluable! |
+| `save_paths`                | **no**   | _none_        | _list(key=value)_ | list of key=value elements, where key is type of storage (`s3` or `local`) and value is path                                                                |
+| `tag_increment_level`       | **no**   | `0`           | `0`, `1`, `2`     | Index of number to increment: `[0]`.`[1]`.`[2]`                                                                                                             |
+| `tag_prefix`                | **no**   | _none_        | _String_          | Fixed tag prefix. If not provided, will be empty. Otherwise will create tag: `[prefix]-[version]`                                                           |
+| `tag_prefix_regex`          | **no**   | _none_        | _String_          | If set, will use matching pattern from branch name to generate prefix. Overwrites `tag_prefix` if matched.                                                  |
+| `log_mode`                  | **no**   | `1`           | `0`, `1`, `2`     | Log mode: 0 - warnings only, 1 - info; 2 - debug                                                                                                            |
 
 
 # Example usage
 
-Usage to tag testing builds
+
+Usage to check current version in s3 and store it localy
+```yaml
+- image: drone-tag
+  name: check-current-version
+  settings:
+    access_key:
+      from_secret: aws_key
+    secret_key:
+      from_secret: aws_secret
+    fetch_only: true
+    fetch_from: s3
+    fetch_path: some-version-bucket:this_namespace/testing/this_name/version
+    save_paths:
+      - local=.tag
+```
+
+Usage to tag testing builds. Version stored in s3,
 ```yaml
 - image: drone-tag
   name: tag-build-for-testing
@@ -31,39 +47,41 @@ Usage to tag testing builds
       from_secret: aws_key
     secret_key:
       from_secret: aws_secret
-    local_path: .version
-    s3_bucket: some-version-bucket
-    s3_path: this_namespace/testing/this_name/version
+    fetch_from: s3
+    fetch_path: some-version-bucket:this_namespace/testing/this_name/version
+    save_paths:
+      - s3=some-version-bucket:this_namespace/testing/this_name/version
     tag_increment_level: 1 # Will increment minor version
     tag_prefix_regex: "^\w+-\d+" # i.e. get jira ticket number like "BMD-123" etc 
 ```
 
-Usage to tag production
+Usage to tag production, version stored localy. Using IAM role instead of credentials. Save version to s3 AND localy
 ```yaml
 - image: drone-tag
   name: tag-build-production
   settings:
-    access_key:
-      from_secret: aws_key
-    secret_key:
-      from_secret: aws_secret
-    local_path: .version
-    s3_bucket: some-version-bucket
-    s3_path: this_namespace/production/this_name/version
+    fetch_from: local
+    fetch_path: .tag
+    save_paths:
+      - s3=some-version-bucket:this_namespace/production/this_name/version
+      - local=.tag
     tag_increment_level: 0 # Will increment major version 
 ```
 
-Example of fetching latest tag number, i.e to use with docker plugin later on
+Example of tagging version by branch prefix. Usable to version feature branches
 
 ```yaml
 - image: drone-tag
-  name: fetch-tag
+  name: tag-feature-qa1
   settings:
-    access_key:
-      from_secret: aws_key
-    secret_key:
-      from_secret: aws_secret
+    fetch_from: s3
+    fetch_path: some-version-bucket:this_namespace/testing/this_name/version
+    save_paths:
+      - s3=some-version-bucket:this_namespace/testing/this_name
+      - local=.tag
     local_path: .tag
-    s3_bucket: some-version-bucket
-    s3_path: this_namespace/this_stage/this_name/version 
+    tag_prefix_regex: "^\w+-\d+" # i.e. get jira ticket number like "BMD-123" etc 
+    tag_prefix: qa1 # default value in case regex not resolveable
+    tag_increment_level: 1 # Will increment minor version
+    s3_use_prefix_as_filename: true
 ```
--- drone-tag/run.sh
+++ drone-tag/run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Errors
+trap "exit $EXIT_CODE" TERM
+export TOP_PID=$$
+
+function killMe()
+{
+  EXIT_CODE=$1
+  kill -s TERM $TOP_PID
+  exit 1
+}
 
 # Open a new file descriptor that redirects to stdout:
 exec 3>&1
@@ -8,15 +18,16 @@ exec 3>&1
 
 _access_key=$PLUGIN_ACCESS_KEY
 _secret_key=$PLUGIN_SECRET_KEY
-_fetch_version=${PLUGIN_FETCH_VERSION:-false}
-_local_path=${PLUGIN_LOCAL_PATH:-".tag"}
-_s3_bucket=$PLUGIN_S3_BUCKET
-_s3_path=$PLUGIN_S3_PATH
+_fetch_only=${PLUGIN_FETCH_ONLY:-false}
 _tag_increment_level=${PLUGIN_TAG_INCREMENT_LEVEL:-0}
 _tag_prefix=$PLUGIN_TAG_PREFIX
 _tag_prefix_regex=$PLUGIN_TAG_PREFIX_REGEX
 _log_mode=${PLUGIN_LOG_MODE:-1}
 _s3_use_prefix_as_filename=${PLUGIN_S3_USE_PREFIX_AS_FILENAME:-false}
+
+_fetch_from=${PLUGIN_FETCH_FROM:-s3}
+_fetch_path=$PLUGIN_FETCH_PATH
+_save_paths_raw=${PLUGIN_SAVE_PATHS}
 
 _log_prefix="[TAGGER]"
 
@@ -49,60 +60,175 @@ log ()
   echo "$(date +"[%Y-%m-%d %H:%M:%S]") $_log_prefix $log_mode_prefix $@" 1>&3
 }
 
+containsElement ()
+{
+  ### usage: containsElement $element $arrayToCheck
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
 
-# Initial checks
-## Check if access and secret key provided   ###### Actualy should use iam role instead
-#if [ -z "$_access_key" ] || [ -z "$_secret_key" ]; then
+function createTempFile()
+{
+  local _filename="/tmp/$(date +%s%N)_${RANDOM}"
+  echo $_filename
+}
 
+function increaseVersion()
+{
+  # Usage: increaseVersion version level
+  local iterator=0
+  local _v=$1
+  local _l=$2
+  # modify correct level of version
+  IFS='.' read -ra VER <<< "$_v"
+  for i in "${VER[@]}"; do
+    if [[ $iterator -eq $_l ]]; then
+      i=$((i+1))
+      VER[$iterator]=$i
+    fi
+    iterator=$((iterator+1))
+  done
+  # join array's elements by "."
+  echo $(IFS=. ; echo "${VER[*]}")
+}
 
-# Check if s3 bucket and path provided
-if [ -z "$_s3_bucket" ] || [ -z "$_s3_path" ]; then
-  log error "No S3 bucket or path provided! Exiting!"
-  exit 1
-fi
+function fetch_version()
+{
+  # Usage fetch_version (s3|local) path
+  # Output: path_to_file_with_fetched_version|path_to_file_with_execution_logs
+  local _from=$1
+  local _source_path=$2
+  # create temp file
+  local _filename=$(createTempFile)
+  local _log_file=$(createTempFile)
+  log debug "Fetching from: ${_from}. Path: $_source_path"
+  exec 4>$_log_file 5>&2
+  case $_from in
+    s3)
+      local s3_bucket=$(echo $_source_path | cut -d":" -f1)
+      local path=$(echo $_source_path | cut -d":" -f2)
+
+      ERROR=$( { aws s3api get-object --bucket $s3_bucket --key $path $_filename 1>&4 ;} 2>&1 )
+      if [ -n "$ERROR" ]; then
+        log error "Failed to download from s3://$s3_bucket:$path; Message:" "$ERROR" 1>&2
+        killMe 3
+      fi
+
+      # Check if there is given file. If not, assume version 0.0.0
+      if [ -e $_filename ]; then
+        log debug "Downloaded S3 object: S3://$_source_path" 1>&4
+        #_version=$(grep -oP "\d+\.\d+\.\d+$" $_local_path)
+      else
+        log debug "S3 object not found: S3://$_source_path; Assuming version 0.0.0" 1>&4
+        echo "0.0.0" > $_filename
+      fi
+      ;;
+    local)
+      if [ ! -e $_source_path ]; then
+        log debug "Local file not exist! path: $_source_path; Assuming version 0.0.0" 1>&4
+        echo "0.0.0" > $_filename
+      else
+        cat $_source_path > $_filename
+      fi
+      ;;
+    *)
+      log error "Wrong _from in fetch_version()! Got: $_from ; Exiting" 1>&4
+      killMe 2
+      ;;
+  esac
+  exec 4>&- 5>&-
+  echo "${_filename};${_log_file}"
+}
+
+function storeVersion()
+{
+  # usage: storeVersion tagged_version type1=path1 type2=path2 ...
+  # Returns - path to log file (containing execution log)
+  local _temp_file=$(createTempFile)
+  local _temp_error=$(createTempFile)
+  local _temp_log=$(createTempFile)
+  local version=$1
+  shift
+  echo $version > $_temp_file
+  exec 4>$_temp_error 5>&2 6>$_temp_log
+  while [ -n "$1" ]; do
+    # arg: type=path
+    local arg=$1
+    shift
+    local type=$(echo $arg | cut -d"=" -f1)
+    local path=$(echo $arg | cut -d"=" -f2)
+    if ! $(containsElement $type s3 local); then
+      log error "Wrong key in save_paths! use one of: s3, local; Argument: $arg" 1>&2
+      killMe 2
+    fi
+    case $type in
+      s3)
+        local s3_bucket=$(echo $path | cut -d":" -f1)
+        local s3_path=$(echo $path | cut -d":" -f2)
+        if [[ $_s3_use_prefix_as_filename == "true" ]]; then
+          s3_path="${s3_path}/${_prefix}"
+        fi
+        log debug "Saving new version in S3://$s3_bucket/$s3_path" 1>&6
+        ERROR=$( { aws s3api put-object --bucket $s3_bucket --key $s3_path --body $_temp_file 1>&6 ;} 2>&1 )
+        if [ -n "$ERROR" ]; then
+          log error "Failed to download from s3://$s3_bucket:$s3_path; Message:" "$ERROR" 1>&2
+          killMe 3
+        fi
+        #log debug "$(aws s3api put-object --bucket $s3_bucket --key $s3_path --body $_temp_file 2>&4)" 1>&6
+        ;;
+      local)
+        cp $_temp_file $path
+        log debug "Stored version in file: $path" 1>&6
+        ;;
+      *)
+        log error "Wrong _type in storeVersion()! Got $type ; Exiting" 1>&2
+        killMe 2
+        ;;
+    esac
+  done
+  exec 4>&- 5>&-
+  if [ -e $_temp_error ] && [ -s $_temp_error ]; then
+    log error $(cat $_temp_error) 1>&2
+    killMe 1
+  fi
+  rm $_temp_file
+  rm $_temp_error
+  echo $_temp_log
+}
 
 # Check if prefix set when _s3_use_prefix_as_filename set on true
 if [[ $_s3_use_prefix_as_filename == "true" ]]; then
   if [ -z "$_tag_prefix" ] && [ -z "$_tag_prefix_regex" ]; then
     log error "While using s3_use_prefix_as_filename=true, you have to set at least one of: tag_prefix_regex or tag_prefix"
-    exit 3
+    killMe 2
   fi
 fi
 
 # Set your IAM Identity:
 if [ -n "$_access_key" ] && [ -n "$_secret_key" ]; then
   log debug "configuring credentials"
-  aws configure set aws_access_key_id "$_access_key"
-  aws configure set aws_secret_access_key "$_secret_key"
+  ERROR=$( { aws configure set aws_access_key_id "$_access_key" ;} 2>&1 )
+  ERROR=$( { aws configure set aws_secret_access_key "$_secret_key" ;} 2>&1 )
+  if [ -n "$ERROR" ]; then
+    log error "Error with aws credentials! Message: $ERROR"
+    killMe 3
+  fi
+fi
+# Check if proper value of `fetch_from`
+if ! $(containsElement $_fetch_from s3 local); then
+  log error "Wrong value of fetch_from! use one of: s3, local; got: $_fetch_from"
+  killMe 2
 fi
 
 # Check STS identity
-ERROR=$(if ! test "$(aws sts get-caller-identity)"; then
-  aws sts get-caller-identity
-fi 2>&1 > /dev/null)
-
-
-# Check if able to connect to s3
-ERROR=$(if ! test "$(aws s3api list-objects --bucket $_s3_bucket)"; then
-  aws s3api list-objects --bucket $_s3_bucket
-fi 2>&1 > /dev/null)
+ERROR=$( { aws sts get-caller-identity  1>/dev/null ;} 2>&1 )
 
 # Print last error
 if [ -n "$ERROR" ]; then
   log error "$ERROR"
-  exit 2
-fi
-
-#_found_bucket=$(aws s3api list-buckets | jq ".Buckets[] | select(.Name == \"$_s3_bucket\")")
-
-#if [ -z "$_found_bucket" ]; then
-#  ERROR="Given bucket ($_s3_bucket) not found!"
-#fi
-
-# Print last error
-if [ -n "$ERROR" ]; then
-  log error "$ERROR"
-  exit 2
+  killMe 3
 fi
 
 # If tag_prefix_regex set, try to get it from branch name or tag name. If succeeded, overwrite tag_prefix
@@ -117,90 +243,45 @@ if [ -n "$_tag_prefix_regex" ]; then
   fi
 fi
 
-# If _s3_use_prefix_as_filename=true but failed to evaluate prefix - fail script
-# Modify s3_path to use prefix as filename
-if [[ $_s3_use_prefix_as_filename == "true" ]]; then
-  if [ -z "$_prefix" ]; then
-    log error "Failed to evaluate prefix while using s3_use_prefix_as_filename. Please consider setting default prefix"
-    exit 3
-  fi
-  _s3_path="${_s3_path}/${_prefix}"
-fi
+# Fetch file
+_out_files=$(fetch_version $_fetch_from $_fetch_path)
 
-# Try to download file
-rm -f $_local_path
-log debug "Fetching object:"
-temp_file="/tmp/$(date +%s)_temp_log"
-exec 4>$temp_file 5>&2
-ERROR=$( { aws s3api get-object --bucket $_s3_bucket --key $_s3_path $_local_path 2>&5 1>&4; } )
-if [ -n "$ERROR" ]; then
-  log error "$ERROR"
-  exit 2
-fi
-log debug "$(cat $temp_file)"
-rm $temp_file
-exec 4>&- 5>&-
+_temp_file=$(echo $_out_files | cut -d";" -f1)
+_temp_log_file=$(echo $_out_files | cut -d";" -f2)
+cat $_temp_log_file
+rm $_temp_log_file
+unset _temp_log_file
 
-# Check if there is given file. If not, assume version 0.0.0
-if [ -e $_local_path ]; then
-  log debug "Downloaded S3 object: S3://$_s3_bucket/$_s3_path"
-  _s3_obj_found=true
-  _version=$(grep -oP "\d+\.\d+\.\d+$" $_local_path)
-else
-  log debug "S3 object not found: S3://$_s3_bucket/$_s3_path"
-  _s3_obj_found=false
-fi
+_version=$(grep -oP "\d+\.\d+\.\d+" $_temp_file)
+rm $_temp_file
 
-if ! $_s3_obj_found || [ -z "$_version" ]; then
+if [ -z "$_version" ]; then
   _version="0.0.0"
 fi
 
 log debug "Last version: $_version"
 
-
-
-#log debug "PWD:"
-#pwd
-#log debug "ls: "
-#ls -la
-#log debug "local path: $_local_path"
-
-# If fetch version set on true, only store outcome in local file and exit
-if [[ $_fetch_version == "true" ]]; then
-  _output="$(echo $_prefix | sed -r 's/(.+)/\1-/')$_version"
-  log info "Current tag: $_output"
-  echo $_output > $_local_path
-  exit 0
+# Increase version:
+if [[ ! $_fetch_only == "true" ]]; then
+  _version=$(increaseVersion $_version $_tag_increment_level)
+  log debug "Current version: $_version"
 fi
 
-# Increment version only if fetching version disabled
-if [[ ! $_fetch_version == "true" ]]; then
-
-  iterator=0
-  # modify correct level of version
-  IFS='.' read -ra VER <<< "$_version"
-  for i in "${VER[@]}"; do
-    if [[ $iterator -eq $_tag_increment_level ]]; then
-      i=$((i+1))
-      VER[$iterator]=$i
-    fi
-    iterator=$((iterator+1))
-  done
-  # join array's elements by "."
-  _version=$(IFS=. ; echo "${VER[*]}")
-fi
 _output="$(echo $_prefix | sed -r 's/(.+)/\1-/')$_version"
-log info "Current tag: $_output"
-echo $_output > $_local_path
+log debug "Output version: $_output"
 
-# If fetch version set on true, nothing more to do
-if [[ $_fetch_version == "true" ]]; then
-  exit 0
+if [[ $_s3_use_prefix_as_filename == "true" ]]; then
+  if [ -z "$_prefix" ]; then
+    log error "Failed to evaluate prefix while using s3_use_prefix_as_filename. Please consider setting default prefix"
+    killMe 2
+  fi
 fi
 
-# save new version in s3 bucket
-log debug "Saving new version in S3://$_s3_bucket/$_s3_path"
-log debug "$(aws s3api put-object --bucket $_s3_bucket --key $_s3_path --body $_local_path)"
+# Split save paths
+IFS=',' read -ra _PATHS <<< "$_save_paths_raw"
+log debug "Paths: ${_PATHS[@]}"
+# Execute storeVersions and read stored logs
+cat $(storeVersion $_output ${_PATHS[@]})
 
 log info exiting
 
